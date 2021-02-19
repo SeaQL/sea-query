34922653628f984e154f2f1d0abb56dac4e7d609
Chris Tsang <tyt2y7@gmail.com>
2021-02-19 13:59:46 +0800

    Postgres: Drop Type

--- src/backend/postgres/types.rs
+++ src/backend/postgres/types.rs
@@ -27,12 +27,36 @@ impl TypeBuilder for PostgresQueryBuilder {
             write!(sql, ")").unwrap();
         }
     }
+
+    fn prepare_type_drop_statement(&self, drop: &TypeDropStatement, sql: &mut SqlWriter, _collector: &mut dyn FnMut(Value)) {
+        write!(sql, "DROP TYPE ").unwrap();
+
+        if drop.if_exists {
+            write!(sql, "IF EXISTS ").unwrap();
+        }
+
+        for name in drop.names.iter() {
+            name.prepare(sql, '"');
+        }
+
+        if let Some(option) = &drop.option {        
+            write!(sql, " ").unwrap();
+            self.prepare_drop_type_opt(&option, sql);
+        }
+    }
 }
 
 impl PostgresQueryBuilder {
     fn prepare_create_as_type(&self, as_type: &TypeAs, sql: &mut SqlWriter) {
         write!(sql, "{}", match as_type {
             TypeAs::Enum => "ENUM",
+        }).unwrap()
+    }
+
+    fn prepare_drop_type_opt(&self, opt: &TypeDropOpt, sql: &mut SqlWriter) {
+        write!(sql, "{}", match opt {
+            TypeDropOpt::Cascade => "CASCADE",
+            TypeDropOpt::Restrict => "RESTRICT",
         }).unwrap()
     }
 }
--- src/extension/postgres/types.rs
+++ src/extension/postgres/types.rs
@@ -20,15 +20,36 @@ pub enum TypeAs {
     // Array,
 }
 
+#[derive(Clone, Default)]
+pub struct TypeDropStatement {
+    pub(crate) names: Vec<Rc<dyn Iden>>,
+    pub(crate) option: Option<TypeDropOpt>,
+    pub(crate) if_exists: bool,
+}
+
+#[derive(Clone)]
+pub enum TypeDropOpt {
+    Cascade,
+    Restrict,
+}
+
 pub trait TypeBuilder {
     /// Translate [`TypeCreateStatement`] into database specific SQL statement.
     fn prepare_type_create_statement(&self, create: &TypeCreateStatement, sql: &mut SqlWriter, collector: &mut dyn FnMut(Value));
+
+    /// Translate [`TypeDropStatement`] into database specific SQL statement.
+    fn prepare_type_drop_statement(&self, create: &TypeDropStatement, sql: &mut SqlWriter, collector: &mut dyn FnMut(Value));
 }
 
 impl Type {
     /// Construct type [`TypeCreateStatement`]
     pub fn create() -> TypeCreateStatement {
         TypeCreateStatement::new()
+    }
+
+    /// Construct type [`TypeDropStatement`]
+    pub fn drop() -> TypeDropStatement {
+        TypeDropStatement::new()
     }
 }
 
@@ -81,10 +102,12 @@ impl TypeCreateStatement {
         self.values_dyn(values.into_iter().map(|c| Rc::new(c) as Rc<dyn Iden>).collect())
     }
 
-    pub fn values_dyn(&mut self, values: Vec<Rc<dyn Iden>>) -> &mut Self {
-        self.values = values;
+    pub fn values_dyn(&mut self, mut values: Vec<Rc<dyn Iden>>) -> &mut Self {
+        self.values.append(&mut values);
         self
     }
+
+    // below are boiler plates
 
     pub fn build<T: TypeBuilder>(&self, type_builder: T) -> (String, Vec<Value>) {
         self.build_ref(&type_builder)
@@ -104,6 +127,103 @@ impl TypeCreateStatement {
     pub fn build_collect_ref<T: TypeBuilder>(&self, type_builder: &T, collector: &mut dyn FnMut(Value)) -> String {
         let mut sql = SqlWriter::new();
         type_builder.prepare_type_create_statement(self, &mut sql, collector);
+        sql.result()
+    }
+
+    /// Build corresponding SQL statement and return SQL string
+    pub fn to_string<T>(&self, type_builder: T) -> String
+        where T: TypeBuilder + QueryBuilder {
+        let (sql, values) = self.build_ref(&type_builder);
+        inject_parameters(&sql, values, &type_builder)
+    }
+}
+
+impl TypeDropStatement {
+
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Drop a type
+    ///
+    /// ```
+    /// use sea_query::{*, extension::postgres::Type};
+    ///
+    /// struct FontFamily;
+    /// 
+    /// impl Iden for FontFamily {
+    ///     fn unquoted(&self, s: &mut dyn std::fmt::Write) {
+    ///         write!(s, "{}", "font_family").unwrap();
+    ///     }
+    /// }
+    /// 
+    /// assert_eq!(
+    ///     Type::drop()
+    ///         .if_exists()
+    ///         .name(FontFamily)
+    ///         .restrict()
+    ///         .to_string(PostgresQueryBuilder),
+    ///     r#"DROP TYPE IF EXISTS "font_family" RESTRICT"#
+    /// );
+    /// ```
+    pub fn name<T: 'static>(&mut self, name: T) -> &mut Self
+        where T: Iden {
+        self.name_dyn(Rc::new(name))
+    }
+
+    pub fn name_dyn(&mut self, name: Rc<dyn Iden>) -> &mut Self {
+        self.names.push(name);
+        self
+    }
+
+    pub fn names<T: 'static>(&mut self, names: Vec<T>) -> &mut Self
+        where T: Iden {
+        self.names_dyn(names.into_iter().map(|c| Rc::new(c) as Rc<dyn Iden>).collect())
+    }
+
+    pub fn names_dyn(&mut self, mut names: Vec<Rc<dyn Iden>>) -> &mut Self {
+        self.names.append(&mut names);
+        self
+    }
+
+    /// Set `IF EXISTS`
+    pub fn if_exists(&mut self) -> &mut Self {
+        self.if_exists = true;
+        self
+    }
+
+    /// Set `CASCADE`
+    pub fn cascade(&mut self) -> &mut Self {
+        self.option = Some(TypeDropOpt::Cascade);
+        self
+    }
+
+    /// Set `RESTRICT`
+    pub fn restrict(&mut self) -> &mut Self {
+        self.option = Some(TypeDropOpt::Restrict);
+        self
+    }
+
+    // below are boiler plates
+
+    pub fn build<T: TypeBuilder>(&self, type_builder: T) -> (String, Vec<Value>) {
+        self.build_ref(&type_builder)
+    }
+
+    pub fn build_ref<T: TypeBuilder>(&self, type_builder: &T) -> (String, Vec<Value>) {
+        let mut params = Vec::new();
+        let mut collector = |v| params.push(v);
+        let sql = self.build_collect_ref(type_builder, &mut collector);
+        (sql, params)
+    }
+
+    pub fn build_collect<T: TypeBuilder>(&self, type_builder: T, collector: &mut dyn FnMut(Value)) -> String {
+        self.build_collect_ref(&type_builder, collector)
+    }
+
+    pub fn build_collect_ref<T: TypeBuilder>(&self, type_builder: &T, collector: &mut dyn FnMut(Value)) -> String {
+        let mut sql = SqlWriter::new();
+        type_builder.prepare_type_drop_statement(self, &mut sql, collector);
         sql.result()
     }
 
