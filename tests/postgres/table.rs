4f6bcf3e151ba853d3d890334cd36a576cb3cf2a
Chris Tsang <tyt2y7@gmail.com>
2021-02-19 14:48:25 +0800

    Postgres: Drop Type tests

--- tests/postgres/table.rs
+++ tests/postgres/table.rs
@@ -207,3 +207,43 @@ fn create_6() {
         r#"CREATE TYPE "font" AS ENUM ('name', 'variant', 'language')"#
     );
 }
+
+#[test]
+fn drop_2() {
+    use sea_query::extension::postgres::Type;
+
+    assert_eq!(
+        Type::drop()
+            .name(Font::Table)
+            .to_string(PostgresQueryBuilder),
+        r#"DROP TYPE "font""#
+    );
+}
+
+#[test]
+fn drop_3() {
+    use sea_query::extension::postgres::Type;
+
+    assert_eq!(
+        Type::drop()
+            .if_exists()
+            .name(Font::Table)
+            .restrict()
+            .to_string(PostgresQueryBuilder),
+        r#"DROP TYPE IF EXISTS "font" RESTRICT"#
+    );
+}
+
+#[test]
+fn drop_4() {
+    use sea_query::extension::postgres::Type;
+
+    assert_eq!(
+        Type::drop()
+            .if_exists()
+            .name(Font::Table)
+            .cascade()
+            .to_string(PostgresQueryBuilder),
+        r#"DROP TYPE IF EXISTS "font" CASCADE"#
+    );
+}
