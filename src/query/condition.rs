f633c50a3454dd3bbce1e84acaef46ef0883f168
Chris Tsang <tyt2y7@gmail.com>
2021-06-04 21:06:06 +0800

    #59 Fix `and_where_option`

--- src/query/condition.rs
+++ src/query/condition.rs
@@ -67,8 +67,7 @@ impl Condition {
 
     /// Add an optional condition to the set.
     ///
-    /// If it's an [`Condition::any`], it will be separated from the others by an `" OR "` in the query. If it's
-    /// an [`Condition::all`], it will be separated by an `" AND "`.
+    /// Shorthand for `if o.is_some() { self.add(o) }`
     #[allow(clippy::should_implement_trait)]
     pub fn add_option<C>(self, other: Option<C>) -> Self
     where
@@ -248,10 +247,27 @@ pub trait ConditionalStatement {
         self.cond_where(other)
     }
 
-    /// And where condition, short hand for `if c.is_some() q.and_where(c)`.
+    /// Optional and where, short hand for `if c.is_some() q.and_where(c)`.
+    ///
+    /// ```
+    /// use sea_query::{*, tests_cfg::*};
+    ///
+    /// let query = Query::select()
+    ///     .column(Glyph::Image)
+    ///     .from(Glyph::Table)
+    ///     .and_where(Expr::col(Glyph::Aspect).is_in(vec![3, 4]))
+    ///     .and_where_option(Some(Expr::col(Glyph::Image).like("A%")))
+    ///     .and_where_option(None)
+    ///     .to_owned();
+    ///
+    /// assert_eq!(
+    ///     query.to_string(MysqlQueryBuilder),
+    ///     r#"SELECT `image` FROM `glyph` WHERE `aspect` IN (3, 4) AND `image` LIKE 'A%'"#
+    /// );
+    /// ```
     fn and_where_option(&mut self, other: Option<SimpleExpr>) -> &mut Self {
         if let Some(other) = other {
-            self.and_or_where(LogicalChainOper::And(other));
+            self.and_where(other);
         }
         self
     }
