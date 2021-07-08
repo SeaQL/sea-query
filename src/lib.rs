7c71ddd0a94d3084c776cb0c30a95d4128583558
Chris Tsang <tyt2y7@gmail.com>
2021-07-08 23:03:06 +0800

    Readme

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 
 //! <div align="center">
 //!
-//!   <img src="https://raw.githubusercontent.com/SeaQL/sea-query/master/docs/SeaQL logo dual.png" width="320"/>
+//!   <img src="https://raw.githubusercontent.com/SeaQL/sea-query/master/docs/SeaQL logo dual.png" width="280"/>
 //!
 //!   <h1>SeaQuery</h1>
 //!
@@ -652,6 +652,23 @@
 //!     r#"DROP INDEX `idx-glyph-aspect` ON `glyph`"#
 //! );
 //! ```
+//! 
+//! ## License
+//! 
+//! Licensed under either of
+//! 
+//! -   Apache License, Version 2.0
+//!     ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+//! -   MIT license
+//!     ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+//! 
+//! at your option.
+//! 
+//! ## Contribution
+//! 
+//! Unless you explicitly state otherwise, any contribution intentionally submitted
+//! for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+//! dual licensed as above, without any additional terms or conditions.
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/SeaQL/sea-query/master/docs/SeaQL icon dark.png"
 )]
