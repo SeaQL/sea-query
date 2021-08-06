4cf1e9a6cdab83548f7516d1b54b68654749ea44
Rene Leveille <rene@nestingsafe.com>
2021-08-06 17:05:58 -0400

    compile fails and named flattening

---
+++ sea-query-derive/tests/pass/flattened_named.rs
@@ -0,0 +1,38 @@
+use sea_query::Iden;
+use strum::{EnumIter, IntoEnumIterator};
+
+#[derive(Iden, EnumIter)]
+enum Asset {
+    Table,
+    Id,
+    AssetName,
+    #[iden(flatten)]
+    Creation {
+        info: CreationInfo,
+    },
+}
+
+#[derive(Iden)]
+enum CreationInfo {
+    UserId,
+    #[iden = "creation_date"]
+    Date,
+}
+
+impl Default for CreationInfo {
+    fn default() -> Self {
+        Self::UserId
+    }
+}
+
+fn main() {
+    // custom ends up being default string which is an empty string
+    let expected = ["asset", "id", "asset_name", "user_id", "creation_date"];
+    Asset::iter()
+        .chain(std::iter::once(Asset::Creation {
+            info: CreationInfo::Date,
+        }))
+        .map(|var| Iden::to_string(&var))
+        .zip(expected)
+        .for_each(|(iden, exp)| assert_eq!(iden, exp))
+}
