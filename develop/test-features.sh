bfe5f0d51f0f54e9b9cec452e732184ace5ad3aa
Chris Tsang <tyt2y7@gmail.com>
2021-08-14 18:31:35 +0800

    Test different features

--- develop/test-features.sh
+++
@@ -1,6 +0,0 @@
-cargo build
-cargo build --features=with-chrono
-cargo build --features=with-json
-cargo build --features=with-rust_decimal
-cargo build --features=with-uuid
-cargo build --all-features
