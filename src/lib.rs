cfae4eab32fcd1a87103cffcf8760315f404656f
Ben Armstead <benarmstead@protonmail.com>
2021-08-24 14:08:25 +0000

    Correct grammar

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@
 //!
 //! ## Introduction
 //!
-//! SeaQuery is query builder to help you construct dynamic SQL queries in Rust.
+//! SeaQuery is a query builder to help you construct dynamic SQL queries in Rust.
 //! You can construct expressions, queries and schema as abstract syntax trees using an ergonomic API.
 //! We support MySQL, Postgres and SQLite behind a common interface that aligns their behaviour where appropriate.
 //!
