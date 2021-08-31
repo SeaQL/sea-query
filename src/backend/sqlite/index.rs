962c5021357234669fd2d852632f390dc83233fa
Billy Chan <ccw.billy.123@gmail.com>
2021-08-31 23:07:41 +0800

    Fix SQLite create table with primary key constraint

--- src/backend/sqlite/index.rs
+++ src/backend/sqlite/index.rs
@@ -2,10 +2,13 @@ use super::*;
 
 impl IndexBuilder for SqliteQueryBuilder {
     fn prepare_table_index_expression(&self, create: &IndexCreateStatement, sql: &mut SqlWriter) {
+        if create.index.name.is_some() {
+            write!(sql, "CONSTRAINT ").unwrap();
+        }
+        self.prepare_index_name(&create.index.name, sql);
+
         self.prepare_index_prefix(create, sql);
         write!(sql, "KEY ").unwrap();
-
-        self.prepare_index_name(&create.index.name, sql);
 
         // self.prepare_index_type(&create.index_type, sql);
 
