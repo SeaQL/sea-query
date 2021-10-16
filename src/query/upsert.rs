67bd1da6eb4eda9058ca4be8a3e00cfe0de3c14e
baoyachi <liaoymxsdl@gmail.com>
2021-10-16 19:12:57 +0800

    change api

--- src/query/upsert.rs
+++ src/query/upsert.rs
@@ -17,23 +17,46 @@ pub struct Upsert<T, A> {
 }
 
 impl<T, A> Upsert<T, A> {
-    fn do_conflict_nothing() -> UpsertExpr {
+    pub fn do_conflict_nothing() -> UpsertExpr {
+        UpsertExpr {}
+    }
+
+    pub fn do_conflict<Target>(target: Target) -> Self {
         todo!()
     }
 
-    fn do_conflict<Target>(target: Target) -> UpsertExpr {
+    pub fn do_conflict_on_constraint<Target>(target: Target) -> Self {
         todo!()
     }
 
-    fn do_conflict_on_constraint<Target>(target: Target) -> Self {
+    pub fn do_nothing(&mut self) -> UpsertExpr {
         todo!()
     }
 
-    fn do_nothing(&mut self) -> UpsertExpr {
-        todo!()
-    }
-
-    fn do_action(self) -> UpsertExpr {
+    pub fn do_action(self) -> UpsertExpr {
         todo!()
     }
 }
+
+#[ignore]
+#[test]
+fn test_on_conflict() {
+    use crate::{tests_cfg::*, *};
+
+    // let query = Query::insert()
+    //     .into_table(Glyph::Table)
+    //     .columns(vec![Glyph::Image])
+    //     .upsert(Upsert::do_conflict_nothing())
+    //     .upsert(Upsert::do_conflict("").do_nothing())
+    //     .upsert(Upsert::do_conflict_on_constraint("").do_action())
+    //     .upsert(Upsert::do_conflict("").do_action())
+    //     .values_panic(vec!["12A".into()])
+    //     .returning(Query::select().column(Glyph::Id).take())
+    //     .to_owned();
+    //
+    // assert_eq!(
+    //     query.to_string(PostgresQueryBuilder),
+    //     r#"INSERT INTO "glyph" ("image") VALUES ('12A') ON CONFLICT ("image") RETURNING "id""#
+    // );
+}
+
