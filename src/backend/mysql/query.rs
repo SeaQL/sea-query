c51272ea2291d82d96f3ea7a1a40c99d8f0efdeb
Billy Chan <ccw.billy.123@gmail.com>
2021-11-08 22:05:50 +0800

    Support `RETURNING` in sea-orm

--- src/backend/mysql/query.rs
+++ src/backend/mysql/query.rs
@@ -1,3 +1,21 @@
 use super::*;
 
-impl QueryBuilder for MysqlQueryBuilder {}
+impl QueryBuilder for MysqlQueryBuilder {
+    fn prepare_returning(
+        &self,
+        returning: &[SelectExpr],
+        sql: &mut SqlWriter,
+        collector: &mut dyn FnMut(Value),
+    ) {
+        if !returning.is_empty() {
+            write!(sql, " RETURNING ").unwrap();
+            returning.iter().fold(true, |first, expr| {
+                if !first {
+                    write!(sql, ", ").unwrap()
+                }
+                self.prepare_select_expr(expr, sql, collector);
+                false
+            });
+        }
+    }
+}
