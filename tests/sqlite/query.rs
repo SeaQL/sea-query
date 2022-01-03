fccdf2a54cd9d6fd5486d411b04b31f105d3103c
Spyros Roum <spyrosr1@gmail.com>
2022-01-03 12:47:03 +0200

    Fix another test

--- tests/sqlite/query.rs
+++ tests/sqlite/query.rs
@@ -840,7 +840,7 @@ fn insert_4() {
             .columns(vec![Glyph::Image])
             .values_panic(vec![chrono::NaiveDateTime::from_timestamp(0, 0).into()])
             .to_string(SqliteQueryBuilder),
-        r#"INSERT INTO `glyph` (`image`) VALUES ('1970-01-01 00:00:00')"#
+        r#"INSERT INTO "glyph" ("image") VALUES ('1970-01-01 00:00:00')"#
     );
 }
 
