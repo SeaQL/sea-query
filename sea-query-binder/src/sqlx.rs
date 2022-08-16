7271c0e975d0df4163ff8841b58bbab7b515641b
baoyachi <liaoymxsdl@gmail.com>
2022-08-16 19:05:04 +0800

    fix

--- sea-query-binder/src/sqlx.rs
+++ sea-query-binder/src/sqlx.rs
@@ -2,24 +2,18 @@ use crate::SqlxValues;
 use sea_query::{query::*, QueryBuilder};
 
 pub trait SqlxBinder {
-    fn build_sqlx<T: QueryBuilder + ?Sized>(&self, query_builder: &T) -> (String, SqlxValues);
-    fn build_any_sqlx<T: QueryBuilder + ?Sized>(&self, query_builder: &T) -> (String, SqlxValues);
+    fn build_sqlx<T: QueryBuilder>(&self, query_builder: &T) -> (String, SqlxValues);
+    fn build_any_sqlx<T: QueryBuilder>(&self, query_builder: &T) -> (String, SqlxValues);
 }
 
 macro_rules! impl_sqlx_binder {
     ($l:ident) => {
         impl SqlxBinder for $l {
-            fn build_sqlx<T: QueryBuilder + ?Sized>(
-                &self,
-                query_builder: &T,
-            ) -> (String, SqlxValues) {
+            fn build_sqlx<T: QueryBuilder>(&self, query_builder: &T) -> (String, SqlxValues) {
                 let (query, values) = self.build(query_builder);
                 (query, SqlxValues(values))
             }
-            fn build_any_sqlx<T: QueryBuilder + ?Sized>(
-                &self,
-                query_builder: &T,
-            ) -> (String, SqlxValues) {
+            fn build_any_sqlx<T: QueryBuilder>(&self, query_builder: &T) -> (String, SqlxValues) {
                 let (query, values) = self.build_any(query_builder);
                 (query, SqlxValues(values))
             }
