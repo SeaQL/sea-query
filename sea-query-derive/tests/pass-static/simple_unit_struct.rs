98bddb2639d3d55f3d4b6c412f27b8815bd1700d
Chris Tsang <chris.2y3@outlook.com>
2023-02-20 20:25:37 +0800

    Impl prepare if all names are valid

--- sea-query-derive/tests/pass-static/simple_unit_struct.rs
+++ sea-query-derive/tests/pass-static/simple_unit_struct.rs
@@ -3,7 +3,23 @@ use sea_query::{Iden, IdenStatic};
 #[derive(Copy, Clone, IdenStatic)]
 pub struct SomeType;
 
+#[derive(Copy, Clone, IdenStatic)]
+#[iden(rename = "Hel`lo")]
+pub struct SomeTypeWithRename;
+
 fn main() {
     assert_eq!(SomeType.to_string(), "some_type");
-    assert_eq!(SomeType.as_str(), "some_type");
+    assert_eq!(SomeTypeWithRename.to_string(), "Hel`lo");
+
+    let mut string = String::new();
+    SomeType.prepare(&mut string, '"');
+    assert_eq!(string, "\"some_type\"");
+
+    let mut string = String::new();
+    SomeTypeWithRename.prepare(&mut string, '"');
+    assert_eq!(string, "\"Hel`lo\"");
+
+    let mut string = String::new();
+    SomeTypeWithRename.prepare(&mut string, '`');
+    assert_eq!(string, "`Hel``lo`");
 }
--- sea-query-derive/tests/pass/simple_unit_struct.rs
+++ sea-query-derive/tests/pass/simple_unit_struct.rs
@@ -3,6 +3,23 @@ use sea_query::Iden;
 #[derive(Copy, Clone, Iden)]
 pub struct SomeType;
 
+#[derive(Copy, Clone, Iden)]
+#[iden(rename = "Hel`lo")]
+pub struct SomeTypeWithRename;
+
 fn main() {
     assert_eq!(SomeType.to_string(), "some_type");
+    assert_eq!(SomeTypeWithRename.to_string(), "Hel`lo");
+
+    let mut string = String::new();
+    SomeType.prepare(&mut string, '"');
+    assert_eq!(string, "\"some_type\"");
+
+    let mut string = String::new();
+    SomeTypeWithRename.prepare(&mut string, '"');
+    assert_eq!(string, "\"Hel`lo\"");
+
+    let mut string = String::new();
+    SomeTypeWithRename.prepare(&mut string, '`');
+    assert_eq!(string, "`Hel``lo`");
 }
