ad8564b9663e5416bdb99db4ee6f53610e75c26d
Esteban Borai <estebanborai@gmail.com>
2023-06-25 23:01:21 -0400

    feat: query builder

--- src/backend/postgres/extension.rs
+++ src/backend/postgres/extension.rs
@@ -1,44 +1,52 @@
-// impl ExtensionBuilder for ExtensionStatement {
-//     fn prepare_extension_statement(&self, sql: &mut dyn SqlWriter) {
-//         match self.operation {
-//             ExtensionOperation::Create => {
-//                 write!(sql, "CREATE EXTENSION ").unwrap();
+use super::*;
+use crate::extension::postgres::*;
 
-//                 if self.if_not_exists {
-//                     write!(sql, "IF NOT EXISTS ").unwrap()
-//                 }
+impl ExtensionBuilder for PostgresQueryBuilder {
+    fn prepare_extension_create_statement(
+        &self,
+        create: &ExtensionCreateStatement,
+        sql: &mut dyn SqlWriter,
+    ) {
+        write!(sql, "CREATE EXTENSION ").unwrap();
 
-//                 write!(sql, "{}", self.name).unwrap();
+        if create.if_not_exists {
+            write!(sql, "IF NOT EXISTS ").unwrap()
+        }
 
-//                 if let Some(schema) = self.schema.as_ref() {
-//                     write!(sql, " WITH SCHEMA {}", schema).unwrap();
-//                 }
+        write!(sql, "{}", create.name).unwrap();
 
-//                 if let Some(version) = self.version.as_ref() {
-//                     write!(sql, " VERSION {}", version).unwrap();
-//                 }
+        if let Some(schema) = create.schema.as_ref() {
+            write!(sql, " WITH SCHEMA {}", schema).unwrap();
+        }
 
-//                 if self.cascade {
-//                     write!(sql, " CASCADE").unwrap();
-//                 }
-//             }
-//             ExtensionOperation::Drop => {
-//                 write!(sql, "DROP EXTENSION ").unwrap();
+        if let Some(version) = create.version.as_ref() {
+            write!(sql, " VERSION {}", version).unwrap();
+        }
 
-//                 if self.if_exists {
-//                     write!(sql, "IF EXISTS ").unwrap();
-//                 }
+        if create.cascade {
+            write!(sql, " CASCADE").unwrap();
+        }
+    }
 
-//                 write!(sql, "{}", self.name).unwrap();
+    fn prepare_extension_drop_statement(
+        &self,
+        drop: &ExtensionDropStatement,
+        sql: &mut dyn SqlWriter,
+    ) {
+        write!(sql, "DROP EXTENSION ").unwrap();
 
-//                 if self.cascade {
-//                     write!(sql, " CASCADE").unwrap();
-//                 }
+        if drop.if_exists {
+            write!(sql, "IF EXISTS ").unwrap();
+        }
 
-//                 if self.restrict {
-//                     write!(sql, "  RESTRICT").unwrap();
-//                 }
-//             }
-//         }
-//     }
-// }
+        write!(sql, "{}", drop.name).unwrap();
+
+        if drop.cascade {
+            write!(sql, " CASCADE").unwrap();
+        }
+
+        if drop.restrict {
+            write!(sql, "  RESTRICT").unwrap();
+        }
+    }
+}
