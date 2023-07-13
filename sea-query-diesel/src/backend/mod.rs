4bf89056017b3c1e32abcec7e5a966c4136d6c33
Emile Fugulin <code@efugulin.com>
2023-07-13 18:54:57 -0400

    feat: add sqlite support for diesel integration

---
+++ sea-query-diesel/src/backend/mod.rs
@@ -0,0 +1,45 @@
+use diesel::backend::Backend;
+use diesel::query_builder::QueryFragment;
+use diesel::result::QueryResult;
+use sea_query::{QueryBuilder, Value};
+
+#[cfg(feature = "mysql")]
+mod mysql;
+
+#[cfg(feature = "postgres")]
+mod postgres;
+
+#[cfg(feature = "sqlite")]
+mod sqlite;
+
+pub trait ExtractBuilder {
+    type Builder: QueryBuilder;
+
+    fn builder() -> Self::Builder;
+}
+
+pub trait TransformValue: Backend {
+    fn transform_value(value: Value) -> QueryResult<Box<dyn QueryFragment<Self> + Send>>;
+}
+
+mod macros {
+    macro_rules! err {
+        ($msg: tt) => {
+            ::diesel::result::Error::SerializationError($msg.into())
+        };
+    }
+
+    macro_rules! bail {
+        ($msg: tt) => {
+            return Err($crate::backend::macros::err!($msg))
+        };
+    }
+
+    macro_rules! build {
+        ($type: ident, $value: expr) => {
+            $crate::value::SeaValue::<::diesel::sql_types::Nullable<$type>, _>::build($value)
+        };
+    }
+
+    pub(crate) use {bail, build, err};
+}
---
+++ sea-query-diesel/src/backend/sqlite.rs
@@ -0,0 +1,91 @@
+use diesel::query_builder::QueryFragment;
+use diesel::result::QueryResult;
+use diesel::sql_types::*;
+use diesel::sqlite::sql_types::*;
+use diesel::sqlite::Sqlite;
+use sea_query::{SqliteQueryBuilder, Value};
+
+use super::macros::{bail, build, err};
+use super::{ExtractBuilder, TransformValue};
+
+impl ExtractBuilder for Sqlite {
+    type Builder = SqliteQueryBuilder;
+
+    fn builder() -> Self::Builder {
+        SqliteQueryBuilder
+    }
+}
+
+impl TransformValue for Sqlite {
+    fn transform_value(value: Value) -> QueryResult<Box<dyn QueryFragment<Self> + Send>> {
+        let transformed = match value {
+            Value::Bool(v) => build!(Bool, v),
+            Value::TinyInt(v) => build!(SmallInt, v.map(i16::from)),
+            Value::SmallInt(v) => build!(SmallInt, v),
+            Value::Int(v) => build!(Integer, v),
+            Value::BigInt(v) => build!(BigInt, v),
+            Value::TinyUnsigned(v) => build!(SmallInt, v.map(i16::from)),
+            Value::SmallUnsigned(v) => build!(Integer, v.map(i32::from)),
+            Value::Unsigned(v) => build!(BigInt, v.map(i64::from)),
+            Value::BigUnsigned(_) => bail!("Sqlite doesn't support BigUnsigned arguments"),
+            Value::Float(v) => build!(Float, v),
+            Value::Double(v) => build!(Double, v),
+            Value::String(v) => build!(Text, v.map(|v| *v)),
+            Value::Char(v) => build!(Text, v.map(|v| v.to_string())),
+            Value::Bytes(v) => build!(Blob, v.map(|v| *v)),
+            #[cfg(feature = "with-chrono")]
+            Value::ChronoDate(v) => build!(Date, v.map(|v| *v)),
+            #[cfg(feature = "with-chrono")]
+            Value::ChronoTime(v) => build!(Time, v.map(|v| *v)),
+            #[cfg(feature = "with-chrono")]
+            Value::ChronoDateTime(v) => build!(Timestamptz, v.map(|v| *v)),
+            #[cfg(feature = "with-chrono")]
+            Value::ChronoDateTimeUtc(v) => build!(Timestamptz, v.map(|v| *v)),
+            #[cfg(feature = "with-chrono")]
+            Value::ChronoDateTimeLocal(t) => build!(Timestamptz, t.map(|t| *t)),
+            #[cfg(feature = "with-chrono")]
+            Value::ChronoDateTimeWithTimeZone(t) => build!(Timestamptz, t.map(|t| *t)),
+            #[cfg(feature = "with-time")]
+            Value::TimeDate(v) => build!(Date, v.map(|t| *t)),
+            #[cfg(feature = "with-time")]
+            Value::TimeTime(v) => build!(Time, v.map(|t| *t)),
+            #[cfg(feature = "with-time")]
+            Value::TimeDateTime(t) => build!(Timestamptz, t.map(|t| *t)),
+            #[cfg(feature = "with-time")]
+            Value::TimeDateTimeWithTimeZone(t) => build!(Timestamptz, t.map(|t| *t)),
+            #[cfg(feature = "with-uuid")]
+            Value::Uuid(v) => build!(Blob, v.map(|v| v.as_bytes().to_vec())),
+            #[cfg(feature = "with-rust_decimal")]
+            Value::Decimal(v) => {
+                use rust_decimal::prelude::ToPrimitive;
+                let v = v
+                    .map(|v| {
+                        v.to_f64()
+                            .ok_or(err!("Decimal cannot be represented as Double"))
+                    })
+                    .transpose()?;
+                build!(Double, v)
+            }
+            #[cfg(feature = "with-bigdecimal")]
+            Value::BigDecimal(v) => {
+                use bigdecimal::ToPrimitive;
+                let v = v
+                    .map(|v| {
+                        v.to_f64()
+                            .ok_or(err!("BigDecimal cannot be represented as Double"))
+                    })
+                    .transpose()?;
+                build!(Double, v)
+            }
+            #[cfg(feature = "with-json")]
+            Value::Json(v) => build!(Text, v.map(|v| v.to_string())),
+            #[cfg(feature = "with-ipnetwork")]
+            Value::IpNetwork(_) => bail!("Sqlite doesn't support IpNetwork arguments"),
+            #[cfg(feature = "with-mac_address")]
+            Value::MacAddress(_) => bail!("Sqlite doesn't support MacAddress arguments"),
+            #[cfg(feature = "postgres-array")]
+            Value::Array(_, _) => bail!("Sqlite doesn't support array arguments"),
+        };
+        Ok(transformed)
+    }
+}
