018571eba008d5dde2ee7239f8d7338d4a8e4848
Stefan Grönke <stefan@gronke.net>
2024-10-14 16:14:23 +0200

    elseif support in IfElseStatement

--- src/backend/mysql/query.rs
+++ src/backend/mysql/query.rs
@@ -147,6 +147,11 @@ impl QueryBuilder for MysqlQueryBuilder {
     fn insert_default_keyword(&self) -> &str {
         "()"
     }
+
+    /// Prefix of the ELSEIF (MySQL)
+    fn elseif_keyword_prefix(&self) -> &str {
+        "ELSE"
+    }
 }
 
 impl MysqlQueryBuilder {
