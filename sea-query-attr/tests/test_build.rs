391ad4e76ccccb4086127dfafb142a2a65991afd
Chris Tsang <chris.2y3@outlook.com>
2025-05-28 23:13:14 +0200

    Delete sea-query-attr

--- sea-query-attr/tests/test_build.rs
+++
@@ -1,8 +0,0 @@
-#[test]
-fn build_tests() {
-    let t = trybuild::TestCases::new();
-    t.compile_fail("./tests/compile-fail/*.rs");
-
-    // all of these are exactly the same as the examples in `examples/derive.rs`
-    t.pass("./tests/pass/*.rs");
-}
