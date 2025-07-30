0b09586be64c9850609b3046a42402c6ca3f05d6
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2025-07-30 13:19:09 +0800

    Update tests/size.rs
    Co-authored-by: Dmitrii Aleksandrov <adk2300@gmail.com>

--- tests/size.rs
+++ tests/size.rs
@@ -1,5 +1,11 @@
 use sea_query::Value;
 
+/// If this check fails, you need to run `benches/value.rs`
+/// and see if the increased `Value` size hurts performance.
+///
+/// If it does, box the big variant.
+///
+/// If it doesn't, bump the constant here in the test.
 #[test]
 fn value_size() {
     let max = 40;
