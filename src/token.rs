5470b79dec803e117b912ea3f4b36770decb4666
Huliiiii <134658521+Huliiiiii@users.noreply.github.com>
2025-08-07 13:41:17 +0800

    if_not_else

--- src/token.rs
+++ src/token.rs
@@ -54,10 +54,10 @@ impl Tokenizer {
             }
             self.inc();
         }
-        if !string.is_empty() {
-            Some(Token::Space(string))
-        } else {
+        if string.is_empty() {
             None
+        } else {
+            Some(Token::Space(string))
         }
     }
 
@@ -77,10 +77,10 @@ impl Tokenizer {
                 break;
             }
         }
-        if !string.is_empty() {
-            Some(Token::Unquoted(string))
-        } else {
+        if string.is_empty() {
             None
+        } else {
+            Some(Token::Unquoted(string))
         }
     }
 
@@ -102,11 +102,11 @@ impl Tokenizer {
                 if self.end() {
                     break;
                 }
-                if !Self::is_string_escape_for(start, self.get()) {
-                    break;
-                } else {
+                if Self::is_string_escape_for(start, self.get()) {
                     write!(string, "{}", self.get()).unwrap();
                     self.inc();
+                } else {
+                    break;
                 }
             } else if !first {
                 escape = !escape && Self::is_escape_char(c);
@@ -116,10 +116,10 @@ impl Tokenizer {
                 break;
             }
         }
-        if !string.is_empty() {
-            Some(Token::Quoted(string))
-        } else {
+        if string.is_empty() {
             None
+        } else {
+            Some(Token::Quoted(string))
         }
     }
 
@@ -140,11 +140,11 @@ impl Tokenizer {
                 if self.end() {
                     break;
                 }
-                if !Self::is_string_escape_for(start, self.get()) {
-                    break;
-                } else {
+                if Self::is_string_escape_for(start, self.get()) {
                     write!(string, "{c}").unwrap();
                     self.inc();
+                } else {
+                    break;
                 }
             } else if !first {
                 escape = !escape && Self::is_escape_char(c);
@@ -166,10 +166,10 @@ impl Tokenizer {
                 self.inc();
             }
         }
-        if !string.is_empty() {
-            Some(Token::Punctuation(string))
-        } else {
+        if string.is_empty() {
             None
+        } else {
+            Some(Token::Punctuation(string))
         }
     }
 
