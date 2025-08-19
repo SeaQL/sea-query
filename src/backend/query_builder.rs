703c50548c3a94a6022ab7b823f7c454bc30804a
honzuki <22032642+honzuki@users.noreply.github.com>
2025-08-19 17:31:24 +0900

    fix feature flag

--- src/backend/query_builder.rs
+++ src/backend/query_builder.rs
@@ -936,6 +936,7 @@ pub trait QueryBuilder:
                 JoinType::LeftJoin => "LEFT JOIN",
                 JoinType::RightJoin => "RIGHT JOIN",
                 JoinType::FullOuterJoin => "FULL OUTER JOIN",
+                #[cfg(feature = "backend-mysql")]
                 JoinType::StraightJoin => "STRAIGHT_JOIN",
             }
         )
