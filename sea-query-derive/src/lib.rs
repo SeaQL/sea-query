013186ac111b4fc1fda0568b3d4595c68412d312
Chris Tsang <chris.2y3@outlook.com>
2025-10-25 15:23:20 +0100

    Make sea-query-derive more lightweight

--- sea-query-derive/src/lib.rs
+++ sea-query-derive/src/lib.rs
@@ -1,26 +1,32 @@
 #![forbid(unsafe_code)]
 
 use proc_macro::TokenStream;
+#[cfg(feature = "sea-query")]
 use syn::{DeriveInput, parse_macro_input};
 
+#[cfg(feature = "sea-query")]
 mod enum_def;
+#[cfg(feature = "sea-query")]
 mod iden;
 mod raw_query;
 mod raw_sql;
 mod sqlx;
 
+#[cfg(feature = "sea-query")]
 #[proc_macro_derive(Iden, attributes(iden, method))]
 pub fn derive_iden(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
     iden::expand(input)
 }
 
+#[cfg(feature = "sea-query")]
 #[proc_macro_derive(IdenStatic, attributes(iden, method))]
 pub fn derive_iden_static(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
     iden::iden_static::expand(input)
 }
 
+#[cfg(feature = "sea-query")]
 #[proc_macro_attribute]
 pub fn enum_def(args: TokenStream, input: TokenStream) -> TokenStream {
     enum_def::expand(args, input)
@@ -34,6 +40,7 @@ pub fn raw_sql(input: TokenStream) -> TokenStream {
     }
 }
 
+#[cfg(feature = "sea-query")]
 #[proc_macro]
 pub fn raw_query(input: TokenStream) -> TokenStream {
     match raw_query::expand(input) {
