2ed646afa78a52e7af4518b147e2fcc6c7961ad7
Chris Tsang <chris.2y3@outlook.com>
2025-11-27 22:12:09 +0000

    fmt

--- src/value/prelude.rs
+++ src/value/prelude.rs
@@ -1,33 +1,33 @@
 
-    #[cfg(feature = "with-json")]
-    pub use serde_json::Value as Json;
+#[cfg(feature = "with-json")]
+pub use serde_json::Value as Json;
 
-    #[cfg(feature = "with-chrono")]
-    pub use chrono::{DateTime, FixedOffset, Local, NaiveDate, NaiveDateTime, NaiveTime, Utc};
+#[cfg(feature = "with-chrono")]
+pub use chrono::{DateTime, FixedOffset, Local, NaiveDate, NaiveDateTime, NaiveTime, Utc};
 
-    #[cfg(feature = "with-time")]
-    pub use time::{OffsetDateTime, PrimitiveDateTime};
+#[cfg(feature = "with-time")]
+pub use time::{OffsetDateTime, PrimitiveDateTime};
 
-    #[cfg(feature = "with-jiff")]
-    pub use jiff::{Timestamp, Zoned};
+#[cfg(feature = "with-jiff")]
+pub use jiff::{Timestamp, Zoned};
 
-    #[cfg(feature = "with-rust_decimal")]
-    pub use rust_decimal::Decimal;
+#[cfg(feature = "with-rust_decimal")]
+pub use rust_decimal::Decimal;
 
-    #[cfg(feature = "with-bigdecimal")]
-    pub use bigdecimal::BigDecimal;
+#[cfg(feature = "with-bigdecimal")]
+pub use bigdecimal::BigDecimal;
 
-    #[cfg(feature = "with-uuid")]
-    pub use uuid::Uuid;
+#[cfg(feature = "with-uuid")]
+pub use uuid::Uuid;
 
-    #[cfg(feature = "with-ipnetwork")]
-    pub use ipnetwork::IpNetwork;
+#[cfg(feature = "with-ipnetwork")]
+pub use ipnetwork::IpNetwork;
 
-    #[cfg(feature = "with-ipnetwork")]
-    pub use std::net::IpAddr;
+#[cfg(feature = "with-ipnetwork")]
+pub use std::net::IpAddr;
 
-    #[cfg(feature = "with-mac_address")]
-    pub use mac_address::MacAddress;
+#[cfg(feature = "with-mac_address")]
+pub use mac_address::MacAddress;
 
-    #[cfg(feature = "postgres-range")]
-    pub use sea_query_postgres_types::range::RangeType;
+#[cfg(feature = "postgres-range")]
+pub use sea_query_postgres_types::range::RangeType;
