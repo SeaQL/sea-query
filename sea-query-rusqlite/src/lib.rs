2462a11fa932fec0a86d0010925353519053b79c
Chris Tsang <chris.2y3@outlook.com>
2025-12-07 00:59:18 +0000

    rusqlite: bind json null as json null

--- sea-query-rusqlite/src/lib.rs
+++ sea-query-rusqlite/src/lib.rs
@@ -55,15 +55,6 @@ impl_rusqlite_binder!(WithQuery);
 
 impl ToSql for RusqliteValue {
     fn to_sql(&self) -> Result<ToSqlOutput<'_>> {
-        macro_rules! box_to_sql {
-            ( $v: expr ) => {
-                match $v {
-                    Some(v) => v.to_sql(),
-                    None => Null.to_sql(),
-                }
-            };
-        }
-
         macro_rules! opt_string_to_sql {
             ( $v: expr ) => {
                 match $v {
@@ -117,7 +108,17 @@ impl ToSql for RusqliteValue {
             #[cfg(feature = "with-uuid")]
             Value::Uuid(v) => v.to_sql(),
             #[cfg(feature = "with-json")]
-            Value::Json(j) => box_to_sql!(j),
+            Value::Json(j) => {
+                if cfg!(feature = "sea-orm") && j.is_some() && j.as_ref().unwrap().is_null() {
+                    // rusqlite binds Json::Null as SQL NULL
+                    // which is different from sqlx
+                    return "null".to_sql();
+                }
+                match j {
+                    Some(v) => v.to_sql(),
+                    None => Null.to_sql(),
+                }
+            }
             #[cfg(feature = "with-rust_decimal")]
             Value::Decimal(v) => opt_string_to_sql!(v.as_ref().map(|v| v.to_string())),
             #[cfg(feature = "with-bigdecimal")]
