06c9248d273decf9f9ff00b0975b44f4eaf2e30c
Huliiiiii <134658521+Huliiiiii@users.noreply.github.com>
2025-12-20 14:25:53 +0800

    Clippy

--- src/utils.rs
+++ src/utils.rs
@@ -4,8 +4,8 @@
 pub(crate) fn join_write<T, B: std::fmt::Write>(
     buf: &mut B,
     items: impl IntoIterator<Item = T>,
-    mut join: impl (FnMut(&mut B) -> std::fmt::Result),
-    mut r#do: impl (FnMut(&mut B, T) -> std::fmt::Result),
+    mut join: impl FnMut(&mut B) -> std::fmt::Result,
+    mut r#do: impl FnMut(&mut B, T) -> std::fmt::Result,
 ) -> std::fmt::Result {
     let mut iter = items.into_iter();
     if let Some(first) = iter.next() {
