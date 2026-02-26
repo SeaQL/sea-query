e42bf9ec9b29171375c7f303b8160287b8a4d85b
Chris Tsang <chris.2y3@outlook.com>
2026-02-26 10:19:44 +0000

    Fix docs

--- src/backend/query_builder.rs
+++ src/backend/query_builder.rs
@@ -22,7 +22,7 @@ pub trait QueryBuilder:
         self.prepare_insert_statement_common(insert, sql);
     }
 
-    /// Common implementation of [`prepare_insert_statement`] shared among backends.
+    /// Common implementation of `prepare_insert_statement` shared among backends.
     fn prepare_insert_statement_common(&self, insert: &InsertStatement, sql: &mut impl SqlWriter) {
         if let Some(with) = &insert.with {
             self.prepare_with_clause(with, sql);
